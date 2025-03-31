db71dad0a256b90f539f1a36f4c15edc465efac3
Alexandr <al3fil4@gmail.com>
2025-03-31 09:53:25 +0500

    users

--- PyTrack/learning/urls.py
+++ PyTrack/learning/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from . import views
 from django.shortcuts import render
-from .views import custom_404
+from .views import custom_404, register, user_login, user_logout
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    #path('', views.index, name='index'),
     path('main/', views.main_page, name='main'),
     path("sub/", lambda request: render(request, 'learning/sub.html'), name='sub'),
     path('about/', views.about_page, name='about'),
@@ -14,7 +14,10 @@ urlpatterns = [
     path('practice3/', views.practice3, name='practice3'),
     path('lesson/<int:lesson_id>/', views.get_lesson, name='get_lesson'),
     path('lesson/<str:lesson_id>/', views.get_lesson, name='get_lesson'),
-    path('404/', views.error404, name='404')
+    path('404/', views.error404, name='404'),
+    path('', register, name='register'),
+    path('login/', user_login, name='login'),
+    path('logout/', user_logout, name='logout'),
 ]
 
 handler404 = custom_404
--- PyTrack/learning/views.py
+++ PyTrack/learning/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 def index(request):
     return render(request, 'learning/index.html', {})
@@ -43,3 +45,29 @@ def custom_404(request, exception):
 
 def error404(request):
     return render(request, "learning/404.html")
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('main')  # Перенаправляем на главную страницу
+    else:
+        form = UserCreationForm()
+    return render(request, 'learning/register.html', {'form': form})
+
+def user_login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('main')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'learning/login.html', {'form': form})
+
+def user_logout(request):
+    logout(request)
+    return redirect('index')
